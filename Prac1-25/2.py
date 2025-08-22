cd1b7bbcc5fc0c83a2f578019a4f55a516e5b22c
Van Nipper <vannipper5@gmail.com>
2025-08-22 09:28:04 -0400

    Minor code improvements

--- Prac1-25/2.py
+++ Prac1-25/2.py
@@ -2,9 +2,7 @@ for _ in range( int( input() ) ):
     line = input()
     e_count = line.lower().count("e")
     g_count = line.lower().count("g")
-
     print(line, "is", end = " ")
-    
     if( e_count > g_count ):
         print("EVIL")
     elif( g_count > e_count ):
--- Prac1-25/3.py
+++ Prac1-25/3.py
@@ -1,33 +1,19 @@
 for _ in range(int(input())):
-    s = input()
-    s = s.replace('(', '( ')
-    s = s.replace(')', ' )')
-    l = s.split()
+    l = input().replace('(', '( ').replace(')', ' )').split()
     while(len(l) > 1):
-        # print(f'List: {l}')
         for ind, val in enumerate(l):
-            # print(ind, val)
             if val == ')':
                 templist = l[(ind - 4):ind]
-                # print(f'Templist {templist}')
-                o1 = int(templist[2])
-                o2 = int(templist[3])
-                op = templist[1]
-                #print(o1, o2, op)
+                o1, o2, op = int(templist[2]), int(templist[3]), templist[1]
                 for _ in range(5):
                     l.pop(ind - 4)
-                #print(list)
                 if op == '+':
                     l.insert(ind - 4, str(o1 + o2))
-                    break
                 elif op == '-':
                     l.insert(ind - 4, str(o1 - o2))
-                    break
                 elif op == '*':
                     l.insert(ind - 4, str(o1 * o2))
-                    break
                 else:
-                    l.insert(ind - 4, str(o1**o2))
-                    break
+                    l.insert(ind - 4, str(o1 ** o2))
+                break
     print(int(l[0]))
-
